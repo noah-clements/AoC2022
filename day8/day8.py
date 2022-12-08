7b950a25b838f594c32747cc3d0ad95a55e77f55
noah-clements <noahclements@gmail.com>
2022-12-08 15:22:19 -0500

    day8 part 1 passed

--- day8/day8.py
+++ day8/day8.py
@@ -1,5 +1,8 @@
 from aocd import data
 import logging
+import numpy as np
+from io import StringIO
+
 
 logging.basicConfig(filename='day8.log', level=logging.DEBUG,
                     format='%(asctime)s - %(levelname)s - %(message)s')
@@ -8,9 +11,35 @@ logging.info('Start of program')
 
 def parse(puzzle_input):
     """Parse input."""
-
+    parsed_input = np.genfromtxt(StringIO(puzzle_input), 
+                                 delimiter=1, autostrip=True,
+                                 dtype=int)
+    logging.debug(parsed_input)
+    return parsed_input
+    
 def part1(parsed_data):
-    """Solve part 1."""
+    # logging.debug()
+    logging.debug(parsed_data.shape)
+    len_x, len_y = parsed_data.shape
+    # the -2 is to not double count the corners
+    visible_trees = (len_x + len_y - 2) * 2  # edges
+    for i in range(1, len_x - 1):
+        for j in range(1, len_y - 1):
+            tree = parsed_data[i, j]
+            row = parsed_data[i,:]
+            col = parsed_data[:,j]
+            logging.debug(f'tree at {(i, j)} is {tree}')
+            logging.debug(f'Row maxes are :{np.amax(row[0:j])} & {np.amax(row[j+1:len_x])}')
+            logging.debug(f'Col maxes are :{np.amax(col[0:i])} & {np.amax(col[i+1:len_y])}')
+                          
+            if (tree > np.amax(row[0:j]) or
+                tree > np.amax(row[j+1:len_x]) or
+                tree > np.amax(col[0:i]) or
+                tree > np.amax(col[i+1:len_y])):
+                logging.debug(f'Tree is visible')
+                visible_trees += 1
+    return visible_trees   
+    
 
 def part2(parsed_data):
     """Solve part 2."""
--- day8/test_day8.py
+++ day8/test_day8.py
@@ -3,6 +3,7 @@
 import pathlib
 import pytest
 import day8 as aoc
+import numpy as np
 
 PUZZLE_DIR = pathlib.Path(__file__).parent
 
@@ -16,15 +17,22 @@ def example2():
     puzzle_input = (PUZZLE_DIR / "example2.txt").read_text().strip()
     return aoc.parse(puzzle_input)
 
-@pytest.mark.skip(reason="Not implemented")
+# @pytest.mark.skip(reason="Not implemented")
 def test_parse_example1(example1):
     """Test that input is parsed properly."""
-    assert example1 == ...
+    assert np.array_equal(example1, 
+                          np.array([                              
+                                [3,0,3,7,3],
+                                [2,5,5,1,2],
+                                [6,5,3,3,2],
+                                [3,3,5,4,9],
+                                [3,5,3,9,0],
+                            ]))
 
-@pytest.mark.skip(reason="Not implemented")
+# @pytest.mark.skip(reason="Not implemented")
 def test_part1_example1(example1):
     """Test part 1 on example input."""
-    assert aoc.part1(example1) == ...
+    assert aoc.part1(example1) == 21
 
 @pytest.mark.skip(reason="Not implemented")
 def test_part2_example1(example1):

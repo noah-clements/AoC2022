accbed37e93a67687132b01ff0ac792458c64c9f
noah-clements <noahclements@gmail.com>
2022-12-13 02:42:09 -0500

    day 12 - deleted bad BFS impl

--- day12/day12.py
+++ day12/day12.py
@@ -21,63 +21,6 @@ def get_location(maze:list[list], char):
             if maze[i][j] == char:
                 return (i, j)
 
-def fix_wandering(path:list, topo):
-    for i in range(len(path)):
-        for j in range(len(path) - 1, i + 1, -1):
-            if math.dist(path[i], path[j]) == 1:
-                ih, iw = path[i]
-                jh, jw = path[j]
-                if (topo[jh][jw] - topo[ih][iw]) <= 1:
-                    logging.debug(f'fixing path {path[i]} and {path[j]}')
-                    # logging.debug(f'fixing path:{path[:i+1] + path[j:]}')
-                    return path[:i+1] + path[j:]
-    return path
-
-def get_max_next_loc(location, grid, path:list, nogos:set):
-    x, y = location
-    current_val = grid[x][y]
-    potential_next = location
-    for nx, ny in ((x+1,y), (x-1,y), (x,y+1), (x,y-1)):
-        if (0 <= nx < len(grid) and 0 <= ny < len(grid[0]) 
-            and (nx, ny) not in path
-            and (nx, ny) not in nogos):
-                if grid[nx][ny] - current_val == 1:
-                    return (nx, ny)
-                elif grid[nx][ny] == current_val:
-                    potential_next = (nx, ny)
-    return potential_next
-    
-    
-def old_part1(topo):
-    starting_loc = get_location(topo, 'S')
-    destination = get_location(topo, 'E')
-    # now replace the 'S' & 'E' characters with their values
-    x,y = starting_loc
-    topo[x][y] = 0
-    x,y = destination
-    topo[x][y] = 25
-    current_loc = starting_loc
-    path = []
-    no_gos = set()
-    while current_loc!= destination:
-        next_loc = get_max_next_loc(current_loc, topo, path, no_gos)
-        if next_loc == current_loc:  # have to backtrack
-            # no_gos.add(current_loc)
-            current_loc = path.pop() if len(path) > 0 else starting_loc
-            logging.debug(f'Backtracking from {next_loc} to {current_loc}')
-        else:
-            path.append(current_loc)
-            current_loc = next_loc
-    more_fixes = True
-    while more_fixes:
-        logging.debug('Going to try to fix the wandering path')
-        new_path = fix_wandering(path, topo)
-        if new_path == path:
-            more_fixes = False
-        else:
-            path = new_path
-    logging.debug(path)
-    return len(path)
 
 def part2(topo):
     """Solve part 2."""

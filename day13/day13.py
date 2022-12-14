7f55633326d1d09218a9ef12b968923b95463d80
noah-clements <noahclements@gmail.com>
2022-12-13 22:21:17 -0500

    Day 13 - completed part2 - merge sort

--- day13/day13.py
+++ day13/day13.py
@@ -1,7 +1,7 @@
 from aocd import data
 import logging
 import ast
-import numpy as np
+from collections import deque
 
 logging.basicConfig(filename='aoc.log', level=logging.DEBUG,
                     format='%(asctime)s - %(levelname)s - %(message)s')
@@ -60,8 +60,7 @@ def order(left, right):
     # if the left & right were same length, 0 == continue
     return 0
 
-def part1(parsed_data):
-    """Solve part 1."""
+def part1(parsed_data):    
     sum_of_indices = 0
     logging.debug(f'There are {len(parsed_data)} pairs')
     for i in range(len(parsed_data)):
@@ -72,16 +71,57 @@ def part1(parsed_data):
             sum_of_indices += i + 1
     return sum_of_indices
         
+def merge_sort(messages):
+    sorted_list = []
+    size = len(messages)
+    if size > 1:
+        mid = size // 2
+        left = messages[:mid]
+        right = messages[mid:]
+        sorted_left = merge_sort(left)
+        sorted_right = merge_sort(right)
+        left_size = len(left)
+        right_size = len(right)
+        p=0
+        q=0
+        while p < left_size and q < right_size:
+            if order(sorted_left[p], sorted_right[q]) > -1:
+                sorted_list.append(sorted_left[p])
+                p += 1
+            else:
+                sorted_list.append(sorted_right[q])
+                q += 1
+        # We ran out of elements either in left_copy or right_copy
+        # so we will go through the remaining elements and add them
+        # to the sorted list
+        while p < left_size:
+            sorted_list.append(sorted_left[p])
+            p += 1
+        while q < right_size:
+            sorted_list.append(sorted_right[q])
+            q += 1  
+    else:
+        sorted_list = messages
+    return sorted_list
 
 def part2(parsed_data):
-    """Solve part 2."""
+    flattened = [message for pair in parsed_data for message in pair]
+    flattened.extend([[[2]],[[6]]])
+    sorted_messages = merge_sort(flattened)
+    logging.debug(f'After merge_sort: ')
+    # print(f'After merge_sort:')
+    for message in sorted_messages:
+        # print(message)
+        logging.debug(message)
+    return (sorted_messages.index([[2]])+1) * (sorted_messages.index([[6]])+1)
+    
 
 def solve(data):
     """Solve the puzzle for the given input."""
     parsed_data = parse(data)
     solution1 = part1(parsed_data)
     # reload - as in pytest, this parsed data is a fixture
-    parsed_data = parse(data)
+    # parsed_data = parse(data)
     solution2 = part2(parsed_data)
 
     return solution1, solution2
--- day13/test_day13.py
+++ day13/test_day13.py
@@ -50,10 +50,10 @@ def test_part1_example1(example1):
     """Test part 1 on example input."""
     assert aoc.part1(example1) == 13
 
-@pytest.mark.skip(reason="Not implemented")
+# @pytest.mark.skip(reason="Not implemented")
 def test_part2_example1(example1):
     """Test part 2 on example input."""
-    assert aoc.part2(example1) == ...
+    assert aoc.part2(example1) == 140
 
 @pytest.mark.skip(reason="Not implemented")
 def test_part2_example2(example2):

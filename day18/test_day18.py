3b851ce88d10d439a53e51c109deb3b8e5f1535a
noah-clements <noahclements@gmail.com>
2022-12-22 03:28:21 -0500

    Day18 - looked up on ChatGPT how to find shared sides. It generated code that was easy to fix. Passed part 1.

---
+++ day18/test_day18.py
@@ -0,0 +1,49 @@
+# test_aoc_template.py
+
+import pathlib
+import pytest
+import day18 as aoc
+
+PUZZLE_DIR = pathlib.Path(__file__).parent
+
+@pytest.fixture
+def example1():
+    puzzle_input = (PUZZLE_DIR / "example1.txt").read_text().strip()
+    return aoc.parse(puzzle_input)
+
+@pytest.fixture
+def example2():
+    puzzle_input = (PUZZLE_DIR / "example2.txt").read_text().strip()
+    return aoc.parse(puzzle_input)
+
+# @pytest.mark.skip(reason="Not implemented")
+def test_parse_example1(example1):
+    """Test that input is parsed properly."""
+    assert example1 == [(2,2,2),
+                        (1,2,2),
+                        (3,2,2),
+                        (2,1,2),
+                        (2,3,2),
+                        (2,2,1),
+                        (2,2,3),
+                        (2,2,4),
+                        (2,2,6),
+                        (1,2,5),
+                        (3,2,5),
+                        (2,1,5),
+                        (2,3,5)]
+
+# @pytest.mark.skip(reason="Not implemented")
+def test_part1_example1(example1):
+    """Test part 1 on example input."""
+    assert aoc.part1(example1) == 64
+
+@pytest.mark.skip(reason="Not implemented")
+def test_part2_example1(example1):
+    """Test part 2 on example input."""
+    assert aoc.part2(example1) == ...
+
+@pytest.mark.skip(reason="Not implemented")
+def test_part2_example2(example2):
+    """Test part 2 on example input."""
+    assert aoc.part2(example2) == ...

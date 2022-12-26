a563976548ffd86efbe07593056f434db082e01d
noah-clements <noahclements@gmail.com>
2022-12-26 02:42:44 -0500

    replaced recursive flood fill with iterative

--- day18/day18.py
+++ day18/day18.py
@@ -3,7 +3,7 @@ import logging
 from functools import wraps, cache
 from time import time
 from operator import itemgetter
-import itertools
+import sys
 
 def measure(func):
     @wraps(func)
@@ -26,9 +26,6 @@ def parse(puzzle_input):
     """Parse input."""
     blocks =  [tuple(int(x) for x in line.strip().split(',')) for line in puzzle_input.splitlines()]
     return blocks
-
-max_x, min_x, max_y, min_y, max_z, min_z = 0,0,0,0,0,0
-water = []
 
 
 # ChatGPT ended up refactoring its first attempt.
@@ -62,84 +59,55 @@ def count_exposed_sides(cubes):
         count += len(sides)
     return count
 
-# @cache
-def flood_fill(cubes, x, y, z):
-    global water
-    # return if at edges
-    if (x < min_x or x == max_x or 
-        y < min_y or y == max_y or 
-        z < min_z or z == max_z):
-        return
-    # been here before or hit a cube
-    if ((x,y,z) in water or (x,y,z) in cubes):
-        return
-
-    water.append((x,y,z))
-
-    # spread out - the fill
-    flood_fill(cubes, x + 1, y, z)
-    flood_fill(cubes, x - 1, y, z)
-    flood_fill(cubes, x, y + 1, z)
-    flood_fill(cubes, x, y - 1, z)
-    flood_fill(cubes, x, y, z + 1)
-    flood_fill(cubes, x, y, z - 1)
-
 
 def find_water_sides(cubes):
-    global max_x, min_x, max_y, min_y, max_z, min_z
-    global water
     max_x = max(cubes, key=itemgetter(0))[0] + 2
-    min_x = min(0, min(cubes, key=itemgetter(0))[0] - 2)
+    min_x = min(cubes, key=itemgetter(0))[0] - 1
     max_y = max(cubes, key=itemgetter(1))[1] + 2
-    min_y = min(0, min(cubes, key=itemgetter(1))[1] - 2)
+    min_y = min(cubes, key=itemgetter(1))[1] - 1
     max_z = max(cubes, key=itemgetter(2))[2] + 2
-    min_z = min(0, min(cubes, key=itemgetter(2))[2] - 2)
+    min_z = min(cubes, key=itemgetter(2))[2] - 1
 
-    flood_fill(cubes, 0,0,0) # flood fill starts with 0,0,0
+    water = [(min_x, min_y, min_z)] # just as we'd start a recursive flood fill
     water_sides = 0
+    visited = [] # dfs cache
 
-    for cube in cubes:
-        x, y, z = cube
-        if ((x-1, y, z) not in cubes and
-            len(set(itertools.product(range(min_x, x-1), [y], [z])) & set(cubes)) == 1):
-            cubes_w_gaps.append(cube)
-        elif ((x+1, y, z) not in cubes and
-              len(set(itertools.product(range(x+2, max_x+1), [y], [z])) & set(cubes)) == 1):
-            cubes_w_gaps.append(cube)
-        elif ((x, y-1, z) not in cubes and
-              len(set(itertools.product([x], range(min_y, y-1), [z])) & set(cubes)) == 1):
-            cubes_w_gaps.append(cube)
-        elif ((x, y+1, z) not in cubes and
-              len(set(itertools.product([x], range(y+2, max_y+1), [z])) & set(cubes)) == 1):
-            cubes_w_gaps.append(cube)
-        elif ((x, y, z-1) not in cubes and
-              len(set(itertools.product([x], [y], range(min_z, z-1))) & set(cubes)) == 1):
-            #   (x,y,z-2) in cubes):
-            cubes_w_gaps.append(cube)
-        elif ((x, y, z+1) not in cubes and
-              len(set(itertools.product([x], [y], range(z+2, max_z+1))) & set(cubes)) == 1):
-            cubes_w_gaps.append(cube)
-    logging.debug(cubes_w_gaps)
-    return cubes_w_gaps
+    # do an iterative flood fill - python max recusion
+    while water:
+        x, y, z = water.pop()
+        if (x, y, z) not in visited:
+            visited.append((x, y, z))
+
+            # stole this efficient loop of flood points from u/alykzandr
+            for nx,ny,nz in [(x + 1, y, z), (x - 1, y, z),
+                             (x, y + 1, z), (x, y - 1, z),
+                             (x, y, z + 1), (x, y, z - 1)]:
+                if (min_x <= nx <= max_x and 
+                    min_y <= ny <= max_y and
+                    min_z <= nz <= max_z):
+                    if (nx,ny,nz) in cubes:
+                        water_sides += 1
+                    else:
+                        water.append((nx,ny,nz))
+    return water_sides
 
 @measure
 def part1(cubes):
     """Solve part 1."""
     return count_exposed_sides(cubes)    
 
-
 @measure
-def part2(cubes):
-    """Solve part 2."""
-    return count_exposed_sides(cubes) - len(find_water_sides(cubes))
+def part2(blocks):
+    # using global var so that 
+    # the recursion memoization cache can be used
+    return find_water_sides(blocks)
 
 def solve(data):
-    """Solve the puzzle for the given input."""
     parsed_data = parse(data)
     solution1 = part1(parsed_data)
     # reload - as in pytest, this parsed data is a fixture
     # parsed_data = parse(data)
-    solution2 = part_2(parsed_data)
+    solution2 = part2(parsed_data)
 
     return solution1, solution2
 
